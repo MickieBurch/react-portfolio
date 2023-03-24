840832fcb29046fa8819be4ac7cfe246215cde14
McKenna <Mckennaburch19@gmail.com>
2023-03-23 19:27:15 -0500

    updated contact section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,16 +1,9 @@
 import React from "react";
-import { projects } from "../data.js";
 import { Timeline, EventTimeline } from "react-tailwind-timeline-component";
-
 
 function Projects() {
   return (
-    <section id="projects" className="text-black-400 bg-white-900 body-font">
-     
-      {/*This creates the initial container on the page */}
-          <div
-            className="flex flex-col md:grid grid-cols-9 mx-auto p-2 text-blue-50"
-          ></div>
+    <section id="projects" className="text-black-400 bg-grey-900 body-font">
 
       {/*This div is what holds the actual projects and the apps ive built section */}
       <div className="container px-2 py-5 mx-auto text-center lg:px-40">
@@ -30,7 +23,8 @@ function Projects() {
       >
         <EventTimeline
           title="Test 1 "
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
+      
           leftSideContent={new Date(2020, 7, 28).toLocaleDateString()}
           lineClassName="border-lime-500"
           iconBackgroundClassName="bg-lime-500 ring-lime-400 ring-2 ring-opacity-75"
